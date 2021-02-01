e5a3d27414504f4c9e75358d0e8293bba58f06e6
lesea16 <codru1993@gmail.com>
2021-01-31 23:56:55 -0600

    master branch , husband change the movie location

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World B23");
         System.out.println("B21 feature SDET learning Git ");
     }
 }
