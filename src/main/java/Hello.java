c8ddf027888235f2224cd814ad04f3e9b9cc4f83
lesea16 <codru1993@gmail.com>
2021-02-01 00:12:38 -0600

    movie2 branch watching on the living room for 10 minutes

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,4 +4,5 @@ public class Hello {
         System.out.println("Hello World B23");
         System.out.println("B21 feature SDET learning Git ");
     }
+    // oh i like this movie who played it ? let me continued
 }
